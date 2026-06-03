de35afe8946ecb0711537b09e4ba0d47f2cc8a78
Fathima-BS <fathim2305@gmail.com>
2026-06-03 19:42:50 +0530

    loginUser done

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -24,7 +24,23 @@ module.exports.getUser=async (req,res) => {
         res.status(200).json(users);
     } catch (error) {
         res.status(500).json({message:"failed to fetch users",error:error.message})
+    } 
+}
+module.exports.loginUser=async(req,res)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
     }
-    
+    const {email,password}=req.body;
+    const user=await userModel.findOne({email}).select('+password');
+    if(!user){
+        return res.status(401).json({message:'Invalid email or password'});
+    }
+    const isMatch=await user.comparePassword(password);
+    if(!isMatch){
+        return res.status(401).json({message:'Invalid email or password'});
+    }
+    const token=user.generateAuthToken();
+    res.status(200).json({token,user});
 }
 
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -7,8 +7,12 @@ router.post('/register',[
     body('email').isEmail().withMessage('Invalid email'),
     body('fullname.firstname').isLength({min:3}).withMessage('First name must be atleast 3 characters long'),
     body('password').isLength({min:6}).withMessage('password must be at least 6 characters long')
-],
-userController.registerUser)
-router.get('/allUser',userController.getUser)
+],userController.registerUser);
+// router.get('/allUser',userController.getUser);
+router.post('/login',[
+    body('email').isEmail().withMessage('Invalid email'),
+    body('password').isLength({min:6}).withMessage('password must be atleast 6 charcaters long')
+],userController.loginUser)
+
 
 module.exports=router;
