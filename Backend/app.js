bc30f42038fed272ed98eea22a4d538792cc3136
fathima-BS <fathim2305@gmail.com>
2026-08-06 18:29:43 +0530

    captain registrtion done

--- Backend/app.js
+++ Backend/app.js
@@ -5,7 +5,8 @@ const cors=require('cors')
 const connectDB=require('./db/db')
 const app=express();
 const cookieParser=require('cookie-parser');
-const userRoutes=require('./routes/user.routes')
+const userRoutes=require('./routes/user.routes');
+const captainRoutes=require('./routes/captain.routes');
 connectDB();
 app.use(cors());
 app.get('/',(req,res)=>{
@@ -15,4 +16,6 @@ app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
 app.use(express.json())
 app.use('/users',userRoutes);
+app.use('/captains',captainRoutes)
+
 module.exports=app;
--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -9,6 +9,10 @@ module.exports.registerUser = async (req, res, next) => {
     return res.status(400).json({ errors: errors.array() });
   }
   const { fullname, email, password } = req.body;
+  const isUserAlready=await userModel.findOne({email});
+  if(isUserAlready){
+    return res.status(400).json({message:'User already exist'});
+  }
   const hashedPassword = await userModel.hashPassword(password);
   const user=await userService.createUser({
     firstname:fullname.firstname,
