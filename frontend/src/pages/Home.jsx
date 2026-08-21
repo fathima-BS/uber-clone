211ba51b692c33bbce7fa4daba77cb3ac7c396d5
fathima-BS <fathim2305@gmail.com>
2026-08-21 23:57:51 +0530

    Build ride selection UI

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,41 +1,41 @@
-import React, { useState,useRef } from 'react'
-import {useGSAP} from '@gsap/react'
+import React, { useState, useRef } from 'react'
+import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import LocationSearchPanel from '../components/LocationSearchPanel'
 
 function Home() {
   const [pickup, setPickup] = useState('')
-  const [destination,setDestination] = useState('')
+  const [destination, setDestination] = useState('')
   const [panelOpen, setPanelOpen] = useState(false)
   const panelRef = useRef(null)
   const panelCloseRef = useRef(null)
 
-  const submitHandler=(e)=>{
+  const submitHandler = (e) => {
     e.preventDefault()
   }
-  useGSAP(function(){
-    if(panelOpen){
-      gsap.to(panelRef.current,{
-        height:'70%',
-        padding:24
+  useGSAP(function () {
+    if (panelOpen) {
+      gsap.to(panelRef.current, {
+        height: '70%',
+        padding: 24
       })
-      gsap.to(panelCloseRef.current,{
-        opacity:1
+      gsap.to(panelCloseRef.current, {
+        opacity: 1
       })
-    }else{
-      gsap.to(panelRef.current,{
-        height:'0%',
-        padding:0
+    } else {
+      gsap.to(panelRef.current, {
+        height: '0%',
+        padding: 0
       })
-      gsap.to(panelCloseRef.current,{
-        opacity:0
+      gsap.to(panelCloseRef.current, {
+        opacity: 0
       })
     }
-  },[panelOpen])
+  }, [panelOpen])
 
 
   return (
-    <div className='h-screen relative'>
+    <div className='h-screen relative overflow-hidden'>
       <img className='w-16 h-6 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="uber-logo" />
       <div className='h-screen w-screen'>
         {/* temporory image */}
@@ -43,41 +43,73 @@ function Home() {
       </div>
       <div className=' flex flex-col justify-end h-screen absolute top-0 w-full'>
         <div className='h-[30%] p-6 bg-white relative'>
-          <h5 ref={panelCloseRef} onClick={()=>{
+          <h5 ref={panelCloseRef} onClick={() => {
             setPanelOpen(false)
           }} className='absolute opacity-0 top-6 right-6 text-2xl'>
             <i className="ri-arrow-down-wide-line"></i>
           </h5>
           <h4 className='text-2xl font-semibold'>Find a trip</h4>
-          <form onSubmit={(e)=>{
+          <form onSubmit={(e) => {
             submitHandler(e)
           }}>
             <div className="line absolute h-16 w-1 top-[45%] left-10 bg-gray-800 rounded-full" ></div>
             <input
-            onClick={()=>{
-              setPanelOpen(true)
-            }}
-            value={pickup}
-            onChange={(e)=>{
-              setPickup(e.target.value)
-            }}
-            className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-3' type="text" placeholder='Add a pick-up location' />
+              onClick={() => {
+                setPanelOpen(true)
+              }}
+              value={pickup}
+              onChange={(e) => {
+                setPickup(e.target.value)
+              }}
+              className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full mt-3' type="text" placeholder='Add a pick-up location' />
             <input
-            onClick={()=>{
-              setPanelOpen(true)
-            }}
-            value={destination}
-            onChange={(e)=>{
-              setDestination(e.target.value)
-            }}
-            className='bg-[#eee] px-12 py-2 text-lg rounded-lg  w-full mt-3' type="text" placeholder='Enter your destination' />
+              onClick={() => {
+                setPanelOpen(true)
+              }}
+              value={destination}
+              onChange={(e) => {
+                setDestination(e.target.value)
+              }}
+              className='bg-[#eee] px-12 py-2 text-lg rounded-lg  w-full mt-3' type="text" placeholder='Enter your destination' />
           </form>
         </div>
-        <div ref={panelRef} className='bg-white h-0 overflow-hidden'>
-            <LocationSearchPanel/>
+        <div ref={panelRef} className='bg-white h-0'>
+          <LocationSearchPanel />
         </div>
       </div>
-      
+      <div className=' w-full fixed z-10 bottom-0  px-3 py-8 bg-white'>
+        <h3 className='text-2xl font-semibold mb-5'>Choose a Ride</h3>
+        <div className='w-full flex border-2 border-gray-300 active:border-black rounded-xl mb-2 p-3 items-center justify-between'>
+          <img className='h-13' src="https://cn-geo1.uber.com/image-proc/crop/resizecrop/udam/format=auto/width=956/height=538/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC91ZGFtLWFzc2V0cy82ZWU1MGMxYi0yMTc0LTRjOTctODNhMS1iZmQ0NTQ0Njg5ZDAucG5n" alt="" />
+          <div className='ml-1 w-1/2'>
+            <h4 className='font-medium text-base'>UberGo <span><i className='ri-user-3-fill'></i>4</span></h4>
+            <h5 className='font-medium text-sm'>2 mins away</h5>
+            <p className='font-normal text-xs '>Affordable, compact rides</p>
+          </div>
+          <h2 className='text-lg font-semibold'>₹195.25</h2>
+        </div>
+
+        <div className='w-full flex border-2 border-gray-300 active:border-black rounded-xl mb-2 p-3 items-center justify-between'>       
+          <img className='h-12' src="https://img.autocarpro.in/autocarpro/4d3ef0c9-c75e-46a3-af25-fab216e0bfe8_Untitled.jpg?w=750&h=490&q=75&c=1" alt="" />
+          <div className='ml-2 w-1/2'>
+            <h4 className='font-medium text-base'>Moto <span><i className='ri-user-3-fill'></i>1</span></h4>
+            <h5 className='font-medium text-sm'>3 mins away</h5>
+            <p className='font-normal text-xs '>Affordable motorcycle rides</p>
+          </div>
+          <h2 className='text-lg font-semibold'>₹65.15</h2>
+        </div>
+
+        <div className='w-full flex border-2 border-gray-300 active:border-black rounded-xl mb-2 p-3 items-center justify-between'>
+          <img className='h-15' src="https://cn-geo1.uber.com/image-proc/crop/resizecrop/udam/format=auto/width=552/height=552/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC91ZGFtLWFzc2V0cy9mYzEwMWZmOC04MWExLTQ2YzMtOTk1YS02N2I0YmJkMmYyYmYuanBn" alt="" />
+          <div className='ml-2 w-1/2'>
+            <h4 className='font-medium text-base'>UberAuto <span><i className='ri-user-3-fill'></i>3</span></h4>
+            <h5 className='font-medium text-sm'>2 mins away</h5>
+            <p className='font-normal text-xs '>Affordable auto rides</p>
+          </div>
+          <h2 className='text-lg font-semibold'>₹118</h2>
+        </div>
+
+      </div>
     </div>
   )
 }
